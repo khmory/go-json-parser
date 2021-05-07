6a4c51af96ccfc218d474993435022f07259d7a2
koheimorii <morii@fenrir.co.jp>
2021-05-07 20:22:19 +0900

    Lexerの数字トークン対応

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -2,6 +2,7 @@ package lexer
 
 import (
 	"errors"
+	"strconv"
 )
 
 type Lexer struct {
@@ -18,6 +19,7 @@ var (
 	charNotFoundError     = errors.New("char is not found")
 	invalidCharacterError = errors.New("invalid character")
 	invalidStringError    = errors.New("missing double quote")
+	invalidNumberError    = errors.New("invalid number")
 )
 
 func NewLexer(json string) *Lexer {
@@ -74,6 +76,116 @@ func getCharacterByCodePoint(l *Lexer) (codePoint string) {
 	return
 }
 
+func getNumberToken(ch string, l *Lexer) (string, error) {
+	number := ch
+
+	var state string
+	switch ch {
+	case "-":
+		state = "MINUS"
+	case "0":
+		state = "INT_ZERO"
+	default:
+		state = "INT"
+	}
+
+	isDigit19 := func(ch string) bool {
+		if ch == "1" || ch == "2" || ch == "3" || ch == "4" || ch == "6" || ch == "7" || ch == "8" || ch == "9" {
+			return true
+		}
+		return false
+	}
+	isDigit := func(ch string) bool {
+		if ch == "0" || ch == "1" || ch == "2" || ch == "3" || ch == "4" || ch == "6" || ch == "7" || ch == "8" || ch == "9" {
+			return true
+		}
+		return false
+	}
+	isExp := func(ch string) bool {
+		return ch == "e" || ch == "E"
+	}
+	for {
+		ch := l.current()
+		switch state {
+		case "INT":
+			if isDigit(ch) {
+				number += l.consume()
+				break
+			}
+			if ch == "." {
+				number += l.consume()
+				state = "DECIMAL_POINT"
+				break
+			}
+			if isExp(ch) {
+				number += l.consume()
+				state = "EXP"
+			}
+			goto EscapeForLoop
+		case "MINUS":
+			if isDigit19(ch) {
+				number += l.consume()
+				state = "INT"
+				break
+			}
+			if ch == "0" {
+				number += l.consume()
+				state = "INT_ZERO"
+				break
+			}
+			goto EscapeForLoop
+		case "INT_ZERO":
+			if ch == "." {
+				number += l.consume()
+				state = "DECIMAL_POINT"
+				break
+			}
+			if isDigit(ch) {
+				return "", invalidNumberError
+			}
+			goto EscapeForLoop
+		case "DECIMAL_POINT":
+			if isDigit(ch) {
+				number += l.consume()
+				state = "DECIMAL_POINT_INT"
+				break
+			}
+			goto EscapeForLoop
+		case "DECIMAL_POINT_INT":
+			if isDigit(ch) {
+				number += l.consume()
+				break
+			}
+			if isExp(ch) {
+				number += l.consume()
+				state = "EXP"
+				break
+			}
+			goto EscapeForLoop
+		case "EXP":
+			if isDigit(ch) || ch == "-" || ch == "+" {
+				number += l.consume()
+				state = "EXP_INT"
+			}
+			goto EscapeForLoop
+		case "EXP_INT":
+			if isDigit(ch) {
+				number += l.consume()
+				break
+			}
+			goto EscapeForLoop
+		default:
+			goto EscapeForLoop
+		}
+	}
+EscapeForLoop:
+	lastNum, _ := strconv.Atoi(number[len(number)-1:])
+	if 0 <= lastNum || lastNum <= 9 {
+		return number, nil
+	}
+	return "", invalidNumberError
+}
+
 func (l *Lexer) GetNextToken() (*Token, error) {
 	for {
 		ch := l.consume()
@@ -102,9 +214,15 @@ func (l *Lexer) GetNextToken() (*Token, error) {
 		case "\"":
 			stringToken, err := getStringToken(l)
 			if err != nil {
-				return &Token{}, invalidStringError
+				return &Token{}, err
 			}
 			return &Token{TokenType: "String", Value: stringToken}, nil
+		case "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+			numberToken, err := getNumberToken(ch, l)
+			if err != nil {
+				return &Token{}, err
+			}
+			return &Token{TokenType: "Number", Value: numberToken}, nil
 		default:
 			return &Token{}, invalidCharacterError
 		}
@@ -119,6 +237,9 @@ func isSkipCharacter(ch string) bool {
 }
 
 func (l *Lexer) current() string {
+	if l.length <= l.position {
+		return "EOF"
+	}
 	return l.json[l.position : l.position+1]
 }
 
