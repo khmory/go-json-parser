38c77cdb1a7b15fb2b9c7282dc3144cf99da8127
koheimorii <morii@fenrir.co.jp>
2021-05-08 15:24:07 +0900

    配列のパース機能を追加する

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -11,11 +11,21 @@ var (
 	parseError = errors.New("Unparsed tokens detected'")
 )
 
+const (
+	STATE_START = "START"
+	STATE_COMMA = "COMMA"
+	STATE_VALUE = "VALUE"
+)
+
 func Parse(json string) (interface{}, error) {
 	lex := lexer.NewLexer(json)
-	ret, err := ParseValue(lex)
+	token, err := lex.GetNextToken()
 	if err != nil {
-		return "", err
+		return nil, err
+	}
+	ret, err := parseValue(lex, token)
+	if err != nil {
+		return nil, err
 	}
 	endCheck, _ := lex.GetNextToken()
 	if endCheck.TokenType == "EOF" {
@@ -26,15 +36,14 @@ func Parse(json string) (interface{}, error) {
 	return "", parseError
 }
 
-func ParseValue(l *lexer.Lexer) (interface{}, error) {
-	token, err := l.GetNextToken()
-	if err != nil {
-		return "", err
-	}
-
+func parseValue(l *lexer.Lexer, token *lexer.Token) (interface{}, error) {
 	switch token.TokenType {
 	case "LeftSquareBracket":
-		// todo
+		mapValue, err := parseArray(l)
+		if err != nil {
+			return nil, err
+		}
+		return mapValue, nil
 	case "LeftCurlyBracket":
 		// todo
 	case "String", "Number", "True", "False", "Null":
@@ -42,4 +51,53 @@ func ParseValue(l *lexer.Lexer) (interface{}, error) {
 	}
 	fmt.Printf("[debug:%v]", token)
 	return "", parseError
+}
+
+func parseArray(l *lexer.Lexer) ([]interface{}, error) {
+	var array []interface{}
+	state := STATE_START
+
+	for {
+		token, err := l.GetNextToken()
+		if err != nil {
+			return nil, err
+		}
+		if token.TokenType == "EOF" {
+			break
+		}
+
+		switch state {
+		case STATE_START:
+			if token.TokenType == "RightSquareBracket" {
+				return array, nil
+			}
+			value, err := parseValue(l, token)
+			if err != nil {
+				return nil, err
+			}
+			array = append(array, value)
+			state = STATE_VALUE
+		case STATE_VALUE:
+			if token.TokenType == "RightSquareBracket" {
+				return array, nil
+			}
+			if token.TokenType == "Comma" {
+				state = STATE_COMMA
+				break
+			}
+			fmt.Println("invalid token in value state")
+			return nil, parseError
+		case STATE_COMMA:
+			value, err := parseValue(l, token)
+			if err != nil {
+				return nil, err
+			}
+			array = append(array, value)
+			state = STATE_VALUE
+		default:
+			fmt.Println("invalid state in array parser")
+			return nil, parseError
+		}
+	}
+	return nil, parseError
 }
